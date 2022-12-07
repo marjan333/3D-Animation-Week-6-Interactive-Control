157048470496812ce8c4e9b4b94574e09a1d7e65
marjan333 <37725791+marjan333@users.noreply.github.com>
2022-12-07 23:16:39 +1100

    Week 6

---
+++ Assets/Scripts/Moving.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Moving : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray casted_ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(casted_ray, out hitInfo))
+            {
+                _agent.SetDestination(hitInfo.point);
+            }
+        }
+    }
+}
